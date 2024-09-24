f1138cd5fdb3e5d268964463fcce78c07f96409e
younis <abdoyounis02@gmail.com>
2024-09-24 20:18:58 +0300

    add rest assured & testNG dependencies

---
+++ src/test/java/com/qacart/TestCase.java
@@ -0,0 +1,21 @@
+package com.qacart;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class TestCase {
+
+    @Test
+    public void test() {
+
+        given()
+                .when()
+                .then();
+
+
+    }
+}
